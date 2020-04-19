4156a363185f1a4aa4b69f335f18f720f5d8b031
deecodez <obarodayo@gmail.com>
2020-04-19 18:08:13 +0100

    bug fixed

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
-function divider(num){
-    let ans =[];
-    for (let number =1; number<=num; number++){
-    if (num % 2 === 0){
-        ans.push("yu");
-    } 
-    if (num % 3 === 0){
-        ans.push("gi");
-    } 
-    
-    if (num % 5 === 0){
-        ans.push("oh");
-    }
+// find the range of the val e.g range(10) [1,2,3,4,5,6,7,8,9,10]
 
-     if (num % 2 ===0 && num % 3 === 0){
-        ans.push("yu-gi");
+function range(val){
+    let arr =[];
+    for (let i = 1; i <= val; i++){
+        arr.push(i)
     }
-     if (num % 2 ===0 && num % 5 === 0){
-        ans.push("yu-oh");
-    }
-     if (num % 3 ===0 && num % 5 === 0){
-        ans.push("gi-oh");
-    }
-     if (num % 2 ===0 && num % 3 === 0 && num % 5 ===0){
-        ans.push("yu-gi-oh");
-    }
-    return ans;
-}
+    return arr
 }
 
 
+//check for the remainder
 
-console.log(divider(4));
-console.log(divider(6));
-console.log(divider(30));
-console.log(divider(10));
+function divisible(num){
+    //if it's divisible by 2, 3 and 5
+    if (num % 2 ===0 && num % 3 === 0 && num % 5 ===0){
+        return "yu-gi-oh"
+    }
+    //if it's divisble by 2 and 3 only
+    else if (num % 2 ===0 && num % 3 === 0){
+        return "yu-gi"
+    } 
+     //if it's divisble by 2 and 5 only
+     else if (num % 2 ===0 && num % 5 === 0){
+        return "yu-oh"
+    }
+    //if it's divisble by 3 and 5 only
+    else if (num % 3 ===0 && num % 5 === 0){
+        return "gi-oh"
+    }
+    //if it's divisble 2 only
+    else if (num % 2 === 0){
+        return "yu"
+    }
+    //if it's divisble 3 only
+    else if (num % 3 === 0){
+        return "gi"
+    }
+    //if it's divisble 5 only
+    else if (num % 5 === 0){
+        return "oh"
+    }
+    //if it's not divisible by any
+    else{
+        return num
+    }
+}
+
+//Run everything
+function calAll(val){
+    let newArr = [];
+    for (let i = 0; i<=range(val).length; i++){
+        newArr.push(divisible(i))
+    }
+    console.log(newArr)
+}
+calAll(6)
